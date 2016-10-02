fff11d6920f00c81187e60edb54d803db0b39157
Łukasz Sutkowski <lsutkowski@egnyte.com>
2016-10-02 21:00:18 +0200

    Shorter code.

--- js/route.js
+++ js/route.js
@@ -9,35 +9,30 @@ console.timeEnd("Map");
 
 export const alignPoints = (cities) => {
     const nearest = server(osrm.nearest.bind(osrm));
-    return Observable.from(cities).map(city => {
-        return nearest({ coordinates: [[city.longitude, city.latitude]] })
-            .map(result => {
-                return {
-                    id: city.id,
-                    name: city.name,
-                    area: city.area,
-                    population: city.population,
-                    location: result.waypoints[0].location
-                }
-            })
-    }).mergeAll();
+    return Observable.from(cities).map(city => nearest({ coordinates: [[city.longitude, city.latitude]] })
+        .map(result => ({
+            id: city.id,
+            name: city.name,
+            area: city.area,
+            population: city.population,
+            location: result.waypoints[0].location
+        }))
+    ).mergeAll();
 };
 
 export const calculateRoutes = (alignedPoints) => {
     const route = server(osrm.route.bind(osrm));
-    return Observable.from(alignedPoints).map(start => {
-        return Observable.from(alignedPoints).filter(destination => destination !== start).map(destination => {
-            return route({ coordinates: [start.location, destination.location], overview: "false" })
-                .map(({ routes:[route] }) => {
-                    return {
-                        start,
-                        destination,
-                        distance: route.distance, // meters
-                        duration: route.duration // seconds
-                    }
-                })
-        }).mergeAll();
-    }).concatAll().scan((acc, v, i) => {
+    return Observable.from(alignedPoints).map(start =>
+        Observable.from(alignedPoints).filter(destination => destination !== start).map(destination =>
+            route({ coordinates: [start.location, destination.location], overview: "false" })
+                .map(({ routes:[route] }) => ({
+                    start,
+                    destination,
+                    distance: route.distance, // meters
+                    duration: route.duration // seconds
+                }))
+        ).mergeAll()
+    ).concatAll().scan((acc, v, i) => {
         console.log(`${(100 * (i + 1) / alignedPoints.length / (alignedPoints.length - 1)).toFixed(2)}%`);
         return v;
     });
